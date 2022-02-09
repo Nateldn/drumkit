5ecc427857e9c3dafcb9a940fed432285902baf8
Nateldn <32700665+Nateldn@users.noreply.github.com>
2022-02-09 00:58:15 +0000

    Add Keydown event listener

--- app.js
+++ app.js
@@ -1,81 +1,126 @@
-
-let openhat = document.getElementById("openhat");
 let hihat = document.getElementById("hihat");
 let snare = document.getElementById("snare");
-let clap = document.getElementById("clap");
 let kick = document.getElementById("kick");
-let ride = document.getElementById("ride");
 let boom = document.getElementById("boom");
-let tink = document.getElementById("tink");
+let clap = document.getElementById("clap");
+let openhat = document.getElementById("openhat");
+let ride = document.getElementById("ride");
 let tom = document.getElementById("tom");
+let tink = document.getElementById("tink");
 
 
-
-document.addEventListener ("keydown", (event) => {
-if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
+window.addEventListener ("keydown", (kp) => {
+console.log(kp)
+if (kp.which == 68 ) { //play key d -
+    hihat.pause()
+    hihat.currentTime = 0
+    hihat.play();
 }
-else if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
-}
-
-else if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
+else if (kp.which == 74 ) { //play key j -
+    snare.pause()
+    snare.currentTime = 0
+    snare.play();
 }
 
-else if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
+else if (kp.which == 70 ) { //play key f -
+    kick.pause()
+    kick.currentTime = 0
+    kick.play();
 }
 
-else if (event.keyCode == 65 ) { //play key a -
+else if (kp.which == 65 ) { //play key a -
     boom.pause()
     boom.currentTime = 0
     boom.play();
 }
 
-else if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
+else if (kp.which == 83 ) { //play key s -
+    clap.pause()
+    clap.currentTime = 0
+    clap.play();
 }
 
-else if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
+else if (kp.which == 71 ) { //play key g -
+    openhat.pause()
+    openhat.currentTime = 0
+    openhat.play();
 }
 
-else if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
+else if (kp.which == 72 ) { //play key h -
+    ride.pause()
+    ride.currentTime = 0
+    ride.play();
 }
 
-else if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
+else if (kp.key == 76 ) { //play key l -
+    tom.pause()
+    tom.currentTime = 0
+    tom.play();
 }
 
-else if (event.keyCode == 65 ) { //play key a -
-    boom.pause()
-    boom.currentTime = 0
-    boom.play();
+else if (kp.key == 75 ) { //play key k -
+    tink.pause()
+    tink.currentTime = 0
+    tink.play();
 }
-
 
 });
 
 
-kbd.addEventListener("click", (clk) => {
 
-
-});
+document.addEventListener("click", (e) => {
+    console.log(e)
+    if (e.which == 68 ) { //play key d -
+        hihat.pause()
+        hihat.currentTime = 0
+        hihat.play();
+    }
+    else if (e.which == 74 ) { //play key j -
+        snare.pause()
+        snare.currentTime = 0
+        snare.play();
+    }
+    
+    else if (e.which == 70 ) { //play key f -
+        kick.pause()
+        kick.currentTime = 0
+        kick.play();
+    }
+    
+    else if (e.which == 65 ) { //play key a -
+        boom.pause()
+        boom.currentTime = 0
+        boom.play();
+    }
+    
+    else if (e.which == 83 ) { //play key s -
+        clap.pause()
+        clap.currentTime = 0
+        clap.play();
+    }
+    
+    else if (e.key == 71 ) { //play key g -
+        openhat.pause()
+        openhat.currentTime = 0
+        openhat.play();
+    }
+    
+    else if (e.which == 72 ) { //play key h -
+        ride.pause()
+        ride.currentTime = 0
+        ride.play();
+    }
+    
+    else if (e.which == 76 ) { //play key l -
+        tom.pause()
+        tom.currentTime = 0
+        tom.play();
+    }
+    
+    else if (e.which == 75 ) { //play key k -
+        tink.pause()
+        tink.currentTime = 0
+        tink.play();
+    }
+    
+    });
